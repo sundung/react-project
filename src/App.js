e06439f56c35e7339dc7cb3b13e4fcb378868705
孙志强 <1181288394@qq.com>
2020-02-02 15:43:19 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-/*
-  应用的根组件
+import React, { Component } from 'react';
+import { Button, message } from 'antd';
+
+/* 
+应用的根组件
 */
-import React from "react"
-import { Button } from 'antd'
-export default class App extends React.Component {
-  // 必须写render渲染函数而且必须返回return一个有jsx语法写的虚拟dom对象
-  render() {
-    return <div><Button type='primary'>你好</Button></div>  //虚拟dom最后会渲染成真实的div标签
+
+export default class App extends Component {
+
+  handleClick = () => {
+    message.success('成功啦')
   }
+  render() {
+    return (
+      <Button type="primary" onClick={this.handleClick}>测试antd</Button>
+    )
+  }
+
 }
