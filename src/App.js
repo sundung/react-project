4dd1dae87d012d1ce0b7860d0c1aceaed02e3204
孙志强 <1181288394@qq.com>
2020-02-02 15:22:28 +0800

    antd配置config-overrides.js文件

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@
   应用的根组件
 */
 import React from "react"
-
+import { Button } from 'antd'
 export default class App extends React.Component {
   // 必须写render渲染函数而且必须返回return一个有jsx语法写的虚拟dom对象
   render() {
-    return <div>App</div>  //虚拟dom最后会渲染成真实的div标签
+    return <div><Button type='primary'>你好</Button></div>  //虚拟dom最后会渲染成真实的div标签
   }
 }
