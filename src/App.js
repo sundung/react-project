6d3d250a203237ef63c9674c55ee9a1329f291bc
孙志强 <1181288394@qq.com>
2020-02-02 13:59:48 +0800

    重写index和App文件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+  应用的根组件
+*/
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  // 必须写render渲染函数而且必须返回return一个有jsx语法写的虚拟dom对象
+  render() {
+    return <div>App</div>  //虚拟dom最后会渲染成真实的div标签
+  }
 }
-
-export default App;
